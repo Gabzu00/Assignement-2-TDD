56623867ec31e8858a7ac2c82a3ba8a68bcbccdf
SamuelAhlvin <ahlvinsamme@gmail.com>
2022-03-01 16:58:18 +0100

    Added diceOne function

--- Assignement-2-TDD/python-template/PIG/Dice.py
+++ Assignement-2-TDD/python-template/PIG/Dice.py
@@ -6,6 +6,13 @@ class Dice:
 
     def throwDice():
         return random.randint(1,6)
+    
 
+    def diceOne():
+        print (" ______")
+        print ("|      |")
+        print ("|   •  |")
+        print ("|______|")
+        print ("You got a one!")
 
 
