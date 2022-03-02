3bf415077fe51e2138398ea077ddd9246f2e7948
Gabriel Petersson <gabriel@littleland.se>
2022-03-02 20:34:26 +0100

    Added cheat class

--- Assignement-2-TDD/python-template/PIG/Main.py
+++ Assignement-2-TDD/python-template/PIG/Main.py
@@ -1,7 +1,12 @@
 """Detta motsvarar vår main -> vår meny"""
 
-
 from Player import Player_class
+
+def main():
+    print("Hello welcome to the PIG game")
+    print("")
+
+
 
 
 player_1 = Player_class("Samuel", 0)
@@ -11,3 +16,5 @@ player_1.show_score()
 player_1.change_score(5)
 player_1.show_score()
 
+if __name__ == "__main__":
+    main()
