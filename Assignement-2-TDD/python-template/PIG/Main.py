925da8a930aad8828a6d154991a604eafb2a0ad4
SamuelAhlvin <ahlvinsamme@gmail.com>
2022-03-02 20:10:46 +0100

    Added player constructor, show_score and change_score

--- Assignement-2-TDD/python-template/PIG/Main.py
+++ Assignement-2-TDD/python-template/PIG/Main.py
@@ -1,7 +1,13 @@
 """Detta motsvarar vår main -> vår meny"""
 
-def HelloWorld():
-    return "Hello World"
+
+from Player import Player_class
 
 
+player_1 = Player_class("Samuel", 0)
+
+
+player_1.show_score()
+player_1.change_score(5)
+player_1.show_score()
 
--- Assignement-2-TDD/python-template/PIG/Player.py
+++ Assignement-2-TDD/python-template/PIG/Player.py
@@ -1,2 +1,16 @@
 """Lägga till ny spelare -> Spara spelare"""
 
+class Player_class:
+    
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    
+
+    def show_score(self):
+        print(self.name, "has", self.score, "points")
+
+
+    def change_score(self, new_score):
+        self.score = new_score
