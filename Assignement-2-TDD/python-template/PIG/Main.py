12056c4f6ce83c2d4414608649bdb1f097f58a9a
Gabriel Petersson <gabriel@littleland.se>
2022-03-02 20:51:33 +0100

    Added instructions in main

--- Assignement-2-TDD/python-template/PIG/Main.py
+++ Assignement-2-TDD/python-template/PIG/Main.py
@@ -2,13 +2,20 @@
 
 from Player import Player_class
 
+
 def main():
-    print("Hello welcome to the PIG game")
+    print("Instructions: ")
+    print("1. Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to hold:")
+    print("2. If the player rolls a 1, they score nothing and it becomes the next player's turn")
+    print("3. If the player rolls any other number, it is added to their turn total and the player's turn continues.")
+    print("4. If a player chooses to hold, their turn total is added to their score, and it becomes the next player's turn.")
+    print("5. The first player to score 100 or more points wins.")
     print("")
-
-
-
-
+    print("-------------------------------------------------------------------------------------------------------------")
+    print("")
+    print("Type cheat if you want to win faster")
+    
+    
 player_1 = Player_class("Samuel", 0)
 
 
