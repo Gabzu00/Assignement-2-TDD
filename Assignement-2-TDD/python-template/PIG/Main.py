c68d742179728577e732897a095753dfd183d564
Gabriel Petersson <gabriel@littleland.se>
2022-03-08 12:41:48 +0100

    test_Dice done

--- Assignement-2-TDD/python-template/PIG/Main.py
+++ Assignement-2-TDD/python-template/PIG/Main.py
@@ -1,32 +1,33 @@
 """Detta motsvarar vår main -> vår meny"""
 
+class startMain():
 
-def main():
-    check = False
-    while check == False:
-        print("Pvp --> press '1'")
-        print("PvE --> press '2'")
-        answer = input()
-        if answer == '1' or answer =='2':
-            check = True
+    def main():
+        check = False
+        while check == False:
+            print("Pvp --> press '1'")
+            print("PvE --> press '2'")
+            answer = input()
+            if answer == '1' or answer =='2':
+                check = True
+            else:
+                print("Invalid input")
+
+    
+        print("\nInstructions: ")
+        print("1. Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to hold:")
+        print("2. If the player rolls a 1, they score nothing and it becomes the next player's turn")
+        print("3. If the player rolls any other number, it is added to their turn total and the player's turn continues.")
+        print("4. If a player chooses to hold, their turn total is added to their score, and it becomes the next player's turn.")
+        print("5. The first player to score 100 or more points wins.")
+        print("6. If you choose to cheat you will immediately get 100 point and win the game\n")
+
+        if answer == '1':
+            import PvP
+            PvP.Start.startPlayer1()
         else:
-            print("Invalid input")
+            import PvE
+            PvE.Start.startPlayer()
 
-   
-    print("\nInstructions: ")
-    print("1. Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to hold:")
-    print("2. If the player rolls a 1, they score nothing and it becomes the next player's turn")
-    print("3. If the player rolls any other number, it is added to their turn total and the player's turn continues.")
-    print("4. If a player chooses to hold, their turn total is added to their score, and it becomes the next player's turn.")
-    print("5. The first player to score 100 or more points wins.")
-    print("6. If you choose to cheat you will immediately get 100 point and win the game\n")
-
-    if answer == '1':
-        import PvP
-        PvP.Start.startPlayer1()
-    else:
-        import PvE
-        PvE.Start.startPlayer()
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
--- Assignement-2-TDD/python-template/test/test_Dice.py
+++ Assignement-2-TDD/python-template/test/test_Dice.py
@@ -40,3 +40,68 @@ class TestDiceClass(unittest.TestCase):
         res = Dice.dice
         exp = res.diceSix()
         self.assertTrue(exp == "⚅ --> It was a six!")
+        
+    def test_one(self):
+        res = Dice.dice
+        exp = res.diceOne()
+        self.assertFalse(exp == "⚀")
+        
+    def test_two(self):
+        res = Dice.dice
+        exp = res.diceTwo()
+        self.assertFalse(exp == "⚁")
+        
+    def test_three(self):
+        res = Dice.dice
+        exp = res.diceThree()
+        self.assertFalse(exp == "⚂")
+        
+    def test_four(self):
+        res = Dice.dice
+        exp = res.diceFour()
+        self.assertNotEqual(exp, "⚃")
+        
+    def test_five(self):
+        res = Dice.dice
+        exp = res.diceFive()
+        self.assertNotEqual(exp, "⚄")
+        
+    def test_six(self):
+        res = Dice.dice
+        exp = res.diceSix()
+        self.assertNotEqual(exp, "⚅")
+        
+    def test_throw_dice1(self):
+        res = Dice.dice
+        exp = res.throwDice()
+        self.assertLess(exp, 7)
+        
+    def test_throw_dice2(self):
+        res = Dice.dice
+        exp = res.throwDice()
+        self.assertLess(exp, 8)
+        
+    def test_throw_dice3(self):
+        res = Dice.dice
+        exp = res.throwDice()
+        self.assertLess(exp, 9)
+        
+    def test_throw_dice4(self):
+        res = Dice.dice
+        exp = res.throwDice()
+        self.assertGreater(exp, 0)
+    
+    def test_throw_dice5(self):
+        res = Dice.dice
+        exp = res.throwDice()
+        self.assertLess(exp, 10)
+        
+    def test_throw_dice6(self):
+        res = Dice.dice
+        exp = res.throwDice()
+        self.assertLess(exp, 11)
+        
+    def test_throw_dice7(self):
+        res = Dice.dice
+        exp = res.throwDice()
+        self.assertLess(exp, 12)
