84462fcb0cde58467d46825dade086a0d9041162
SamuelAhlvin <ahlvinsamme@gmail.com>
2022-03-09 15:51:06 +0100

    test_PvE changes

--- Assignement-2-TDD/python-template/test/test_PvE.py
+++ Assignement-2-TDD/python-template/test/test_PvE.py
@@ -2,6 +2,10 @@
 from tabnanny import check
 import unittest
 from PIG import PvE
+import io
+import sys
+import os
+
 
 class TestPvEClass(unittest.TestCase):
 
@@ -10,7 +14,16 @@ class TestPvEClass(unittest.TestCase):
         self.assertEqual(res, 0)
 
     def test_init(self):
-        res = PvE.Start
-        exp = res.init(check)
-        self.assertEqual(exp, False)
-        
+        res = PvE.Start.init()
+        exp = PvE.Start.init()
+        self.assertEqual(exp, res)
+
+    #def test_startPlayer(self):
+    #    capturedOutput = sys.stdout
+    #    sys.stdout = io.StringIO()
+    #    res = PvE.Start
+    #    res.startPlayer()
+    #    sys.stdout = sys.__stdout__
+    #    exp = res.difficulty
+
+    #    self.assertTrue(res == 0)
