02ed0b5ea00d2c2133cf31fe3ae82fb99cb63db0
Gabriel Petersson <gabriel@littleland.se>
2022-03-10 14:52:08 +0100

    Updated release file

--- Assignement-2-TDD/python-template/test/test_Main.py
+++ Assignement-2-TDD/python-template/test/test_Main.py
@@ -1,10 +1,24 @@
-# import unittest
-# from PIG import Main
-# import sys
+"""Tester för PvE klassen."""
+import unittest 
+from unittest import mock
+from PIG import Main
+import io
+from unittest.mock import patch
 
-# class TestMainClass(unittest.TestCase):
+class TestPvEClass(unittest.TestCase):
     
-#     def test_foo(self):                           
-#         Main.startMain.main()                                   
-#         sys.stdout = sys.__stdout__                 
-#         print("Test av output", sys.stdout)
+    @patch('sys.stdout', new_callable=io.StringIO) 
+    def test_startPlayer1End(self, mock_stdout):
+        myInstance = Main.startMain
+        with mock.patch('builtins.input', side_effect=["2", "1", "Gabriel", "Q"]):
+            myInstance.main()
+        
+        self.assertTrue("Instructions" in mock_stdout.getvalue())
+        
+    @patch('sys.stdout', new_callable=io.StringIO) 
+    def test_startPlayer2End(self, mock_stdout):
+        myInstance = Main.startMain
+        with mock.patch('builtins.input', side_effect=["1", "n1", "n2", "Q"]):
+            myInstance.main()
+        
+        self.assertTrue("End" in mock_stdout.getvalue())
